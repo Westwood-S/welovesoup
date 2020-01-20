ee7dc729e444c5b77fd0f33cc76f3979b00aa663
Ovidiu Mura <ovymura@yahoo.com>
2020-01-19 21:30:06 -0800

    adding a moving drone

--- src/welovesoup/Drone.java
+++ src/welovesoup/Drone.java
@@ -9,16 +9,23 @@ public class Drone extends Unit {
 
     public void takeTurn() throws GameActionException {
         super.takeTurn();
-        RobotInfo[] robotInfos = rc.senseNearbyRobots();
-        for(RobotInfo r: robotInfos) {
-            if(r.type == RobotType.COW) {
-                if(rc.canPickUpUnit(r.getID())) {
-                    rc.pickUpUnit(r.getID());
-                    nav.goTo(new MapLocation(5,5));
-                    rc.dropUnit(Direction.SOUTH);
-                }
-                System.out.println("drone delivered a cow to enemy");
-            }
-        }
+        Direction direction = Util.randomDirection();
+        while(!nav.goTo(direction))
+            direction = Util.randomDirection();
+        System.out.println("Moving drone");
+//        RobotInfo[] robotInfos = rc.senseNearbyRobots();
+//        for(RobotInfo r: robotInfos) {
+//            if(r.type == RobotType.COW) {
+//                while(!nav.goTo(Util.randomDirection())) ;
+//                System.out.println("Moving a COOWW");
+////                if(rc.canPickUpUnit(r.getID())) {
+////                    System.out.println("^^^^^^^^^^^^^^^^^^^^^^^");
+////                    rc.pickUpUnit(r.getID());
+////                    nav.goTo(new MapLocation(5,5));
+////                    rc.dropUnit(Direction.SOUTH);
+////                }
+//                System.out.println("drone delivered a cow to enemy");
+//            }
+//        }
     }
 }
