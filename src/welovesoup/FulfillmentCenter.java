826e41a9771537b2eaefb4c61cf527316e9aab6d
Ovidiu Mura <ovymura@yahoo.com>
2020-01-22 00:41:33 -0800

    adding new features to drone: move around a point in a circle, catch objects and throw them in the flood

--- src/welovesoup/FulfillmentCenter.java
+++ src/welovesoup/FulfillmentCenter.java
@@ -18,13 +18,16 @@ public class FulfillmentCenter extends Building {
         // will only actually happen if we haven't already broadcasted the creation
         comms.broadcastFulfillmentCenterCreation(rc.getLocation());
         Direction dir = Util.randomDirection();
-        while (numDrone<2) {
-            while (!tryBuild(RobotType.DELIVERY_DRONE, dir)) {
-                dir = Util.randomDirection();
-            }
-            numDrone++;
-            System.out.println("made a delivery drone");
-            dir = Util.randomDirection();
+        while (numDrone<5) {
+            //for (Direction dir : Util.directions) {
+                while (!tryBuild(RobotType.DELIVERY_DRONE, dir)) {
+                    dir = Util.randomDirection();
+                }
+                    numDrone++;
+
+                System.out.println("made a delivery drone");
+                //dir = Util.randomDirection();
+            //}
         }
     }
 }
