cffded023761a07c33b15c4c9a474f2a3918059c
Ovidiu Mura <ovymura@yahoo.com>
2020-02-12 21:23:06 -0800

    adding testAllLandScapers jUnit method in HQTest class

--- test/welovesoup/HQTest.java
+++ test/welovesoup/HQTest.java
@@ -1,11 +1,15 @@
 package welovesoup;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import battlecode.common.*;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+
+import java.util.ArrayList;
 
 public class HQTest {
 
@@ -20,4 +24,19 @@ public class HQTest {
 		assertEquals(0, hq.numMiners);
 	}
 
+	@Test
+	public void testAllLandScapers() {
+		RobotInfo[] robots = new RobotInfo[1];
+		HQ hq = Mockito.mock(HQ.class);
+		doAnswer((i)->{
+			if(robots.length < 8)
+				return false;
+			else
+				return true;
+		}).when(hq).AllLandScapers(robots);
+		int i = robots.length;
+		hq.AllLandScapers(robots);
+		verify(hq,times(1)).AllLandScapers(robots);
+		assertEquals(hq.AllLandScapers(robots), false);
+	}
 }
