e035d83d608a1594d261abecbbba8d28ad71d8ef
Ovidiu Mura <ovymura@yahoo.com>
2020-02-12 19:26:30 -0800

    adding testTakeTurn() unit testing

--- test/welovesoup/BuildingTest.java
+++ test/welovesoup/BuildingTest.java
@@ -1,11 +1,12 @@
 package welovesoup;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.*;
 import battlecode.common.*;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 
 public class BuildingTest {
 
@@ -14,5 +15,17 @@ public class BuildingTest {
         Building building1 = Mockito.mock(Building.class);
         Building building2 = Mockito.mock(Building.class);
         assertNotEquals(building1, building2);
+    }
+
+    @Test
+    public void testTakeTurn() throws GameActionException {
+        Building building = Mockito.mock(Building.class);
+        doAnswer((i)->{
+            return building.turnCount+=1;
+        }).when(building).takeTurn();
+        int i = building.turnCount;
+        building.takeTurn();
+        verify(building,times(1)).takeTurn();
+        assertEquals(building.turnCount, i+1);
     }
 }
