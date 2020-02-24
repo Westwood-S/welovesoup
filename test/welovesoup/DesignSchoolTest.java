7421725d7d69a1296351651861cf4f38d88deac4
Ovidiu Mura <ovymura@yahoo.com>
2020-02-24 12:44:54 -0800

    adding junit for getRoundnum test

--- test/welovesoup/DesignSchoolTest.java
+++ test/welovesoup/DesignSchoolTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import battlecode.common.*;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 public class DesignSchoolTest {
@@ -25,5 +26,26 @@ public class DesignSchoolTest {
         ds.takeTurn();
         verify(ds,times(1)).takeTurn();
         assertEquals(ds.turnCount, i+1);
+    }
+
+    @Test
+    public void testTakeTurn_getRoundNum() throws GameActionException {
+        DesignSchool ds = Mockito.mock(DesignSchool.class);
+        ds.rc = Mockito.mock(RobotController.class);
+        ds.numLandScapers = 1;
+        doAnswer((i)->{
+            if(ds.rc.getRoundNum() < 550 && ds.numLandScapers < 8)
+                ds.numLandScapers += 1;
+            return ds.numLandScapers;
+        }).when(ds).takeTurn();
+        when(ds.rc.getRoundNum()).thenReturn(500);
+        int i = ds.numLandScapers;
+        ds.takeTurn();
+        verify(ds,times(1)).takeTurn();
+        assertEquals(i+1, ds.numLandScapers);
+        when(ds.rc.getRoundNum()).thenReturn(550);
+        ds.takeTurn();
+        verify(ds,times(2)).takeTurn();
+        assertEquals(i+1, ds.numLandScapers);
     }
 }
