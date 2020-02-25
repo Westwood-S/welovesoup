14b2e61352faf0d1254ec31afca484a354f47019
Ovidiu Mura <mura@pdx.edu>
2020-02-25 08:43:33 -0800

    Update DesignSchoolTest.java
    adding new method

--- test/welovesoup/DesignSchoolTest.java
+++ test/welovesoup/DesignSchoolTest.java
@@ -50,4 +50,66 @@ public class DesignSchoolTest {
         verify(ds,times(2)).takeTurn();
         assertEquals(i+1, ds.numLandScapers);
     }
+    
+        @Test
+    public void testTakeTurn_tryBuild_directions() throws GameActionException {
+        DesignSchool ds = Mockito.mock(DesignSchool.class);
+        ds.rc = Mockito.mock(RobotController.class);
+        ds.numLandScapers = 0;
+        doAnswer((i)->{
+            if(ds.rc.getRoundNum() < 550 && ds.numLandScapers < 8) {
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.CENTER)).thenReturn(true);
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.EAST)).thenReturn(true);
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.NORTH)).thenReturn(true);
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.WEST)).thenReturn(true);
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.SOUTH)).thenReturn(true);
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.SOUTHEAST)).thenReturn(true);
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.NORTHEAST)).thenReturn(true);
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.NORTHWEST)).thenReturn(true);
+                when(ds.tryBuild(RobotType.LANDSCAPER, Direction.SOUTHWEST)).thenReturn(true);
+            }
+            return ds.numLandScapers;
+        }).when(ds).takeTurn();
+        // do increment the numLandScapers
+        when(ds.rc.getRoundNum()).thenReturn(500);
+        int i = ds.numLandScapers;
+        ds.takeTurn();
+        int actual = 0;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.SOUTH) == true)
+            actual += 1;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.CENTER) == true)
+            actual += 1;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.SOUTHWEST) == true)
+            actual += 1;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.NORTHWEST) == true)
+            actual += 1;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.EAST) == true)
+            actual += 1;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.NORTH) == true)
+            actual += 1;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.SOUTHEAST) == true)
+            actual += 1;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.NORTHEAST) == true)
+            actual += 1;
+        if (ds.tryBuild(RobotType.LANDSCAPER, Direction.WEST) == true)
+            actual += 1;
+        assertEquals(9, actual);
+        ds.numLandScapers = actual;
+        verify(ds,times(1)).takeTurn();
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.SOUTH);
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.SOUTHEAST);
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.SOUTHWEST);
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.NORTH);
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.NORTHEAST);
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.NORTHWEST);
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.EAST);
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.WEST);
+        verify(ds,times(1)).tryBuild(RobotType.LANDSCAPER, Direction.CENTER);
+        assertEquals(i+9, ds.numLandScapers);
+        // don't increment numLandScapers
+        when(ds.rc.getRoundNum()).thenReturn(550);
+        ds.takeTurn();
+        verify(ds,times(2)).takeTurn();
+        assertEquals(i+9, ds.numLandScapers);
+    }
 }
