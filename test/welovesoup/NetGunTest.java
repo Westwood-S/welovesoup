9db778a1e4fcc25af6f3e21b18a0c514f25ae990
Ovidiu Mura <ovymura@yahoo.com>
2020-02-27 10:44:06 -0800

    adding new test files

--- test/welovesoup/NetGunTest.java
+++ test/welovesoup/NetGunTest.java
@@ -1,9 +1,17 @@
 package welovesoup;
 
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.*;
 
 public class NetGunTest {
 
@@ -12,5 +20,16 @@ public class NetGunTest {
         netGun ng1 = Mockito.mock(netGun.class);
         netGun ng2 = Mockito.mock(netGun.class);
         assertNotEquals(ng1, ng2);
+    }
+
+    @Test
+    public void testTakeTurn() throws GameActionException {
+        netGun n = Mockito.mock(netGun.class);
+        n.rc = Mockito.mock(RobotController.class);
+        doAnswer((i)->{
+            return true;
+        }).when(n).takeTurn();
+        n.takeTurn();
+        verify(n, times(1)).takeTurn();
     }
 }
